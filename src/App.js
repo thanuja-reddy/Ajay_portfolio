f3aaee474dff939a3d568fbb52744e4494c482d7
Thanuja Aalavala <thanuja.a@valuebound.com>
2025-04-28 19:01:36 +0530

    Added dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
